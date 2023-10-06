aea923bb14e90adcb8af79dbafe8138031047d81
titho93 <titho93.10@gmail.com>
2023-10-06 14:22:38 +0200

    [add]email with api

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+      email: "",
+    };
+  },
+
+  methods: {
+    getApi() {
+      axios.get(this.apiUrl).then((answer) => {
+        this.email = answer.data.response;
+      });
+    },
+  },
+
+  mounted() {
+    this.getApi();
   },
 }).mount("#app");
